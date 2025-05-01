53c8b5e8e508c9536123249b15670a8be64d354e
rimagrouaz <153613619+rimagrouaz@users.noreply.github.com>
2025-05-01 23:16:33 +0100

    Update generate-content.py

--- generate-content.py
+++ generate-content.py
@@ -1,6 +1,6 @@
 from google import genai
 
-client = genai.Client(api_key="AIzaSyCKUEncSveW66mkjJUTjKLiPpAaRvd-OHQ")
+client = genai.Client(api_key="your_api_key")
 
 myfile = client.files.upload(file="gemini-native-image.png")
 
